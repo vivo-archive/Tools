7dca217536921dac3a8dc9a6936ddecceb7a032d
nskaggs <nskaggs@8d54b258-bb70-4df8-ae49-98ba5baa8db3>
2010-06-04 16:26:53 +0000

    Correct license headers
    git-svn-id: http://svn.code.sf.net/p/vivo/tools/code@13 8d54b258-bb70-4df8-ae49-98ba5baa8db3

--- VIVO Harvesters/trunk/src/org/vivoweb/ingest/harvest/PubmedSOAPFetch.java
+++ VIVO Harvesters/trunk/src/org/vivoweb/ingest/harvest/PubmedSOAPFetch.java
@@ -1,23 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2010 Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams
- * 
- * This file is part of VIVO.
- * 
- * VIVO is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * VIVO is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with VIVO.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+ * Copyright (c) 2010 Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the new BSD license
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/bsd-license.html
  * 
  * Contributors:
- *     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial implementation
+ *     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
  ******************************************************************************/
 package org.vivoweb.ingest.harvest;
 
