68d2e72a3b87395d6a1763d7f56373be37818f11
massugugo <massugugo@8d54b258-bb70-4df8-ae49-98ba5baa8db3>
2010-06-18 20:06:00 +0000

    XMLRecordOutputStream is still being built
    git-svn-id: http://svn.code.sf.net/p/vivo/tools/code@36 8d54b258-bb70-4df8-ae49-98ba5baa8db3

--- VIVO Harvesters/branches/Utils/src/org/vivoweb/ingest/util/XMLRecordOutputStream.java
+++ VIVO Harvesters/branches/Utils/src/org/vivoweb/ingest/util/XMLRecordOutputStream.java
@@ -1,5 +1,6 @@
 package org.vivoweb.ingest.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -8,18 +9,57 @@ import java.io.OutputStream;
  *
  */
 public class XMLRecordOutputStream extends OutputStream {
+	private ByteArrayOutputStream tempBuf;
+	private ByteArrayOutputStream recBuf;
+	private ByteArrayOutputStream buf;
+	private RecordHandler rh;
+	private boolean inRecord;
+	private byte[] openTag;
+	private byte[] closeTag;
 	
 	/**
 	 * Constructor
+	 * @param tagToSplitOn 
+	 * @param recordHandler 
 	 */
-	public XMLRecordOutputStream() {
-		// TODO Auto-generated constructor stub
+	public XMLRecordOutputStream(String tagToSplitOn, RecordHandler recordHandler) {
+		this.tempBuf = new ByteArrayOutputStream();
+		this.recBuf = new ByteArrayOutputStream();
+		this.buf = this.tempBuf;
+		this.rh = recordHandler;
+		this.inRecord = false;
+		this.closeTag = ("</"+tagToSplitOn+">").getBytes();
+		this.openTag = ("<"+tagToSplitOn).getBytes();
 	}
 
 	@Override
 	public void write(int arg0) throws IOException {
-		// TODO Auto-generated method stub
-		
+		this.buf.write(arg0);
+		byte[] a = this.buf.toByteArray();
+		if(!inRecord() && compareByteArrays(a,this.openTag)) {
+			
+			this.buf = this.recBuf;
+		}else if(inRecord() && compareByteArrays(a, this.closeTag)) {
+			
+			this.buf = this.tempBuf;
+		}
+	}
+	
+	private boolean inRecord() {
+		return (this.buf == this.recBuf);
+	}
+	
+	private boolean compareByteArrays(byte[] a, byte[] b) {
+		if(a.length < b.length) {
+			return false;
+		}
+		int o = a.length-b.length;
+		for(int i = 0; i < b.length; i++) {
+			if(a[o+i] != b[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 }
