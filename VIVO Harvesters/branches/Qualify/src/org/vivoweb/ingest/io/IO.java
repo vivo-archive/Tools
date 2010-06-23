f33a377a499f04ebe5e819da2202fc92f6fa266f
massugugo <massugugo@8d54b258-bb70-4df8-ae49-98ba5baa8db3>
2010-06-23 21:00:17 +0000

    Qualify Branch
    git-svn-id: http://svn.code.sf.net/p/vivo/tools/code@43 8d54b258-bb70-4df8-ae49-98ba5baa8db3

---
+++ VIVO Harvesters/branches/Qualify/src/org/vivoweb/ingest/io/IO.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the new BSD license
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/bsd-license.html
+ * 
+ * Contributors:
+ *     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
+ ******************************************************************************/
+package org.vivoweb.ingest.io;
+
+import java.util.Map;
+
+public class IO {
+	
+	public static void fetch(String host, int port, Map<String, String> params) {
+
+	}
+	
+	public static void translate(String host, int port, Map<String, String> params) {
+
+	}
+	
+	public static void score(String host, int port, Map<String, String> params) {
+
+	}
+	
+}
