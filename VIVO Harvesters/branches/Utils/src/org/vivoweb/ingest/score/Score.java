384c5d5a3fce50b56bfe87f80c989ed18dc5f851
massugugo <massugugo@8d54b258-bb70-4df8-ae49-98ba5baa8db3>
2010-06-24 18:30:56 +0000

    Integrate Score into Utils
    git-svn-id: http://svn.code.sf.net/p/vivo/tools/code@58 8d54b258-bb70-4df8-ae49-98ba5baa8db3

--- VIVO Harvesters/branches/Utils/src/org/vivoweb/ingest/score/Score.java
+++ VIVO Harvesters/branches/Utils/src/org/vivoweb/ingest/score/Score.java
@@ -7,12 +7,13 @@
  * 
  * Contributors:
  *     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
+ *     Christoper Barnes, Narayan Raum - scoring ideas and algorithim
+ *     Yang Li - pairwise scoring algorithm
  ******************************************************************************/
 package org.vivoweb.ingest.score;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
@@ -26,12 +27,32 @@ import com.hp.hpl.jena.rdf.model.*;
  *  @author Nicholas Skaggs nskaggs@ichp.ufl.edu
  */
 public class Score {
-	
 	private static Log log = LogFactory.getLog(Score.class);
 	
 		private Model vivo;
 		private Model scoreInput;
 		private Model scoreOutput;
+		private Model tempModel;
+		
+		final static public void main(String[] args) {
+			//pass models from command line
+			//TODO proper args handler
+			
+			//args[0];
+			//args[1];
+			//debug
+			//Hardcode parms for now
+			
+			//debug
+			
+			//load up models
+			
+			JenaIO vivo = new JenaIO();
+			JenaIO scoreInput = new JenaIO();
+			JenaIO scoreOutput = new JenaIO();
+			
+			new Score(vivo.getJenaModel(),scoreInput.getJenaModel(), scoreOutput.getJenaModel()).execute();
+	    }
 		
 		public Score (Model vivo, Model scoreInput, Model scoreOutput) {
 			this.vivo = vivo;
@@ -41,15 +62,46 @@ public class Score {
 		
 		public void execute() {		
 			 	log.info("Scoring: Start");
+			 	
+			 	ResultSet scoreInputResult;
+			 	ResultSet matchResult;
+			 	
+			 	//DEBUG
+				 	//TODO howto pass this in via config
+				 	String matchAttribute = "email";
+				 	String matchQuery = "PREFIX score: <http://vivoweb.org/ontology/score#> " +
+			    						"SELECT ?x ?email " +
+			    						"WHERE { ?x score:workEmail ?email}";
+				 	String coreAttribute = "core:workEmail";
+				 	String authorAttribute = "author";
+			 	//DEBUG
+			 	
 				//Attempt Matching
+
+			 	//Exact Matches
+			 	//TODO finish implementation of exact matching loop
+			 	//for each matchAttribute
+			 		scoreInputResult = executeQuery(scoreInput, matchQuery);
+			 		exactMatch(vivo,scoreInput,matchAttribute,coreAttribute,scoreInputResult);
+			    //end for
+			 		
+		 		//DEBUG
+				 	//TODO howto pass this in via config
+				 	matchAttribute = "author";
+				 	matchQuery = "PREFIX score: <http://vivoweb.org/ontology/score#> " +
+			    	       		 "SELECT ?x ?author " +
+			    				 "WHERE { ?x score:author ?author}";
+				 	coreAttribute = "core:author";
+			 	//DEBUG
 				
-				//Exact Matches
-				matchEmail(vivo,scoreInput,scoreOutput);
-				
-				//Partial Matches
-				
+				//Pairwise Matches
+				//TODO finish implementation of pairwise matching loop
+			 	//for each matchAttribute
+			 		scoreInputResult = executeQuery(scoreInput, matchQuery);
+			 		pairwiseScore(vivo,scoreInput,matchAttribute,coreAttribute,scoreInputResult);	
+			 	//end for
+			 		
 				//Close and done
-				//TODO change 
 				scoreInput.close();
 		    	scoreOutput.close();
 		    	vivo.close();
@@ -60,8 +112,8 @@ public class Score {
 		 * executeQuery
 		 * Executes a sparql query against a JENA model and returns a result set
 		 * 
-		 * @param  model a model containing statements
-		 * @param  queryString the query to execute against the model
+		 * @param  Model model a model containing statements
+		 * @param  String queryString the query to execute against the model
 		 * @return queryExec the executed query result set
 		 */
 		 private static ResultSet executeQuery(Model model, String queryString) {
@@ -71,31 +123,24 @@ public class Score {
 		    	return queryExec.execSelect();
 			}
 		 
-		 /**
-		 * matchEmail
-		 * Executes an email matching algorithm for author disambiguation
-		 * Returns nothing
-		 * 
-		 * 
-		 * @param  vivo a model containing statements describing authors
-		 * @param  scoreInput a model containing statements to be disambiguated
-		 * @param  scoreOutput a model used to output the disambiguated statements(can be empty)
-		 */
 		 
-		 //TODO refactor to returning a Model of matched statements, and don't require a scoreOutput model
-		 private static void matchEmail(Model vivo, Model scoreInput, Model scoreOutput) {
-				String scoreEmail;
-				String queryString;
-				RDFNode emailNode;
-				RDFNode paperNode;
+		/**
+		 * commitResultSet
+		 * Commits resultset to a vivo model and a score model
+		 * 
+		 * @param  Model vivo a model containing vivo statements
+		 * @param  Model score a model containing vivo + scoring statements
+		 * @param  ResultSet storeResult the result to be stored
+		 * @param  Resource paperResource
+		 * @param  RDFNode matchNode
+		 * @param  RDFNode paperNode
+		 */
+		 private static void commitResultSet(Model vivo, Model score, ResultSet storeResult, Resource paperResource, RDFNode matchNode, RDFNode paperNode) {
 				RDFNode authorNode;
-	            ResultSet scoreInputResult;
-				ResultSet vivoResult;
-				QuerySolution scoreSolution;
 				QuerySolution vivoSolution;
 				StmtIterator paperStmts;
 				Resource authorship;
-				Resource paperResource;
+
                 Property linkedAuthorOf = ResourceFactory.createProperty("http://vivoweb.org/ontology/core#linkedAuthor");
                 Property authorshipForPerson = ResourceFactory.createProperty("http://vivoweb.org/ontology/core#authorInAuthorship");
                 
@@ -108,59 +153,30 @@ public class Score {
                 Property rdfLabel = ResourceFactory.createProperty("http://www.w3.org/2000/01/rdf-schema#label");
 				
                 Statement stmt;
-                
-                //Issue select for email addresses from score_input store
-		    	log.trace("Grabbing all email addresses from input");
-		    	
-		    	queryString =
-		    		"PREFIX score: <http://vivoweb.org/ontology/score#> " +
-		    		"SELECT ?x ?email " +
-		    		"WHERE { ?x score:workEmail ?email}";
-
-		    	scoreInputResult = executeQuery(scoreInput, queryString);
-		    	
-		    	//look for email address in vivo
-		    	while (scoreInputResult.hasNext()) {
-		    		scoreSolution = scoreInputResult.nextSolution();
-	                emailNode = scoreSolution.get("email");
-	                paperNode = scoreSolution.get("x");
-	                paperResource = scoreSolution.getResource("x");
-	                
-	                scoreEmail = emailNode.toString();
-	                
-	                log.trace("\nChecking for " + scoreEmail + " from " + paperNode.toString() + " in VIVO");
-	    			
-	                //Select all matching email addresses from vivo store
-	    			queryString =
-						"PREFIX core: <http://vivoweb.org/ontology/core#> " +
-						"SELECT ?x " +
-						"WHERE { ?x core:workEmail \"" +  scoreEmail + "\" }";
-	    			
-	    			vivoResult = executeQuery(vivo, queryString);
-
-	    	    	//look for email address in VIVO
-	    	    	while (vivoResult.hasNext()) {
-	    	    		vivoSolution = vivoResult.nextSolution();
-	    	    		
-	    	    		//Grab person URI  		    	
-	                    authorNode = vivoSolution.get("x");
-	                    log.info("Found " + scoreEmail + " for person " + authorNode.toString() + " in VIVO");
-	                    log.info("Adding paper " + paperNode.toString() + " to VIVO");
-
-	                    
-	                    //loop through and add statements
-	                    paperStmts = paperResource.listProperties();
-	                    
-	                    //insert paper statements into vivo
-	                    //TODO refactor this to a recursive method
-	                    while (paperStmts.hasNext()) {
-	                    	stmt = paperStmts.nextStatement();
-	                    	log.trace("Paper Statement " + stmt.toString());
-
-	                    	//write to vivo, minus scoring info
-	                    	if (!stmt.getPredicate().toString().contains("/score")) {
-	                    		vivo.add(stmt);
-	                    		                    	
+			 
+				//loop thru vivo matches
+	 	    	while (storeResult.hasNext()) {
+	 	    		vivoSolution = storeResult.nextSolution();
+	 	    		
+	 	    		//Grab person URI
+	                 authorNode = vivoSolution.get("x");
+	                 log.info("Found " + matchNode.toString() + " for person " + authorNode.toString() + " in VIVO");
+	                 log.info("Adding paper " + paperNode.toString() + " to VIVO");
+	
+	                 
+	                 //loop through and add statements
+	                 paperStmts = paperResource.listProperties();
+	                 
+	                 //insert paper statements into vivo
+	                 //TODO refactor this to a recursive method
+	                 while (paperStmts.hasNext()) {
+	                 	stmt = paperStmts.nextStatement();
+	                 	log.trace("Paper Statement " + stmt.toString());
+	
+	                 	//write to vivo, minus scoring info
+	                 	if (!stmt.getPredicate().toString().contains("/score")) {
+	                 		vivo.add(stmt);
+	                 		                    	
 		                    	if (stmt.getObject().isResource()) {
 		                    		StmtIterator objectIterator = ((Resource)stmt.getObject()).listProperties();
 		                    		
@@ -173,36 +189,142 @@ public class Score {
 		                    			}
 		                    		}
 		                    	}
-	                    	}
-	                    	
-	    	    			//write out to score output
-	                    	//TODO add score of 100
-	                    	scoreOutput.add(stmt);
-	                    }
-	                    
-	                    //link author to paper
-	                    log.trace("Link paper " + paperNode.toString() + " to person " + authorNode.toString() + " in VIVO");
-	                    //authorship = ResourceFactory.createResource("http://vivoweb.org/ontology/pubMed/#");
-	                    authorship = ResourceFactory.createResource(paperNode.toString() + "/vivoAuthorship/1");
-	                    //System.out.println(authorship.getNameSpace());
-	                    
-	                    vivo.add(authorship,linkedAuthorOf,authorNode);
-	                    log.trace("Link Statement [" + authorship.toString() + ", " + linkedAuthorOf.toString() + ", " + authorNode.toString() + "]");
-	                    vivo.add((Resource)authorNode,authorshipForPerson,authorship);
-	                    log.trace("Link Statement [" + authorNode.toString() + ", " + authorshipForPerson.toString() + ", " + authorship.toString() + "]");
-	                    vivo.add(authorship,paperOf,paperNode);
-	                    log.trace("Link Statement [" + authorship.toString() + ", " + paperOf.toString() + ", " + paperNode.toString() + "]");
-	                    vivo.add((Resource)paperNode,authorshipForPaper,authorship);
-	                    log.trace("Link Statement [" + paperNode.toString() + ", " + authorshipForPaper.toString() + ", " + authorship.toString() + "]");
-	                    vivo.add(authorship,rdfType,flag1);
-	                    log.trace("Link Statement [" + authorship.toString() + ", " + rdfType.toString() + ", " + flag1.toString() + "]");
-	                    vivo.add(authorship,rdfLabel,"Authorship for Paper");
-	                    log.trace("Link Statement [" + authorship.toString() + ", " + rdfLabel.toString() + ", " + "Authorship for Paper]");
+	                 	}
+	                 	
+	 	    			//write out to score output
+	                 	//TODO add score of 100
+	                 	score.add(stmt);
+	                 }
+	                 
+	               //link author to paper
+                    log.trace("Link paper " + paperNode.toString() + " to person " + authorNode.toString() + " in VIVO");
+                    authorship = ResourceFactory.createResource(paperNode.toString() + "/vivoAuthorship/1");
+                    
+                    vivo.add(authorship,linkedAuthorOf,authorNode);
+                    log.trace("Link Statement [" + authorship.toString() + ", " + linkedAuthorOf.toString() + ", " + authorNode.toString() + "]");
+                    vivo.add((Resource)authorNode,authorshipForPerson,authorship);
+                    log.trace("Link Statement [" + authorNode.toString() + ", " + authorshipForPerson.toString() + ", " + authorship.toString() + "]");
+                    vivo.add(authorship,paperOf,paperNode);
+                    log.trace("Link Statement [" + authorship.toString() + ", " + paperOf.toString() + ", " + paperNode.toString() + "]");
+                    vivo.add((Resource)paperNode,authorshipForPaper,authorship);
+                    log.trace("Link Statement [" + paperNode.toString() + ", " + authorshipForPaper.toString() + ", " + authorship.toString() + "]");
+                    vivo.add(authorship,rdfType,flag1);
+                    log.trace("Link Statement [" + authorship.toString() + ", " + rdfType.toString() + ", " + flag1.toString() + "]");
+                    vivo.add(authorship,rdfLabel,"Authorship for Paper");
+                    log.trace("Link Statement [" + authorship.toString() + ", " + rdfLabel.toString() + ", " + "Authorship for Paper]");
+	
+					 //take results and store in VIVO
+	                 vivo.commit();
+	             }	 
+		 }
+		 
+		 
+		/**
+		* pairwiseScore
+		* Executes a pair scoring method, utilizing the matchAttribute. This attribute is expected to 
+		* return 2 to n results from the given query. This "pair" will then be utilized as a matching scheme 
+		* to construct a sub dataset. This dataset can be scored and stored as a match 
+		* 
+		* 
+		* @param  Model matched a model containing statements describing known authors
+		* @param  Model scoreInput a model containing statements to be disambiguated
+		* @param  String matchAttribute an attribute to perform the exact match
+	    * @param  String coreAttribute an attribute to perform the exact match against from core ontology
+		* @param  ResultSet matchResult contains a resultset of the matchAttribute
+		* @return Model scoreInput
+		*/
+		
+		private static Model pairwiseScore(Model matched, Model scoreInput, String matchAttribute, String coreAttribute, ResultSet matchResult) {			
+		 	//iterate thru scoringInput pairs against matched pairs
+		 	//TODO support partial scoring, multiples matches against several pairs
+		 	//if pairs match, store publication to matched author in Model
+			//TODO return scoreInput minus the scored statements
+			
+			String scoreMatch;
+			String queryString;
+			Resource paperResource;
+			RDFNode matchNode;
+			RDFNode paperNode;
+			ResultSet vivoResult;
+			QuerySolution scoreSolution;
 
-		    			//take results and store in VIVO
-		    			vivo.commit();
-		    			scoreOutput.commit();
-	                }
+		 	//create pairs of *attribute* from matched
+	    	log.trace("Creating pairs of " + matchAttribute + " from input");
+	    	
+	    	//look for exact match in vivo
+	    	while (matchResult.hasNext()) {
+	    		scoreSolution = matchResult.nextSolution();
+                matchNode = scoreSolution.get(matchAttribute);
+                
+                scoreMatch = matchNode.toString();
+                
+                log.trace("\nChecking for " + scoreMatch + " in VIVO");
+    			
+                //Select all matching attributes from vivo store
+    			queryString =
+					"PREFIX core: <http://vivoweb.org/ontology/core#> " +
+					"SELECT ?x " +
+					"WHERE { ?x " + coreAttribute + "\"" +  scoreMatch + "\"}";
+    			
+    			//TODO how to combine result sets? not possible in JENA
+    			vivoResult = executeQuery(vivo, queryString);
+    			commitResultSet(vivo,scoreInput,vivoResult,paperResource,paperNode,matchNode);
+            }	    			 
+	    	
+	    	//TODO return scoreInput minus the scored statements			
+			return scoreInput;
+		 }
+		 
+		 
+		 /**
+		 * exactMatch
+		 * Executes an exact matching algorithm for author disambiguation
+		 * 
+		 * 
+		 * @param  Model vivo a model containing statements describing authors
+		 * @param  Model scoreInput a model containing statements to be disambiguated
+		 * @param  String matchAttribute an attribute to perform the exact match
+		 * @param  String coreAttribute an attribute to perform the exact match against from core ontology
+		 * @param  ResultSet matchResult contains a resultset of the matchAttribute
+		 * @return scoreInput
+		 */
+		 private static Model exactMatch(Model vivo, Model scoreInput, String matchAttribute, String coreAttribute, ResultSet matchResult) {
+				String scoreMatch;
+				String queryString;
+				Resource paperResource;
+				RDFNode matchNode;
+				RDFNode paperNode;
+				ResultSet vivoResult;
+				QuerySolution scoreSolution;
+
+                
+		    	log.trace("Looping thru " + matchAttribute + " from input");
+		    	
+		    	//look for exact match in vivo
+		    	while (matchResult.hasNext()) {
+		    		scoreSolution = matchResult.nextSolution();
+	                matchNode = scoreSolution.get(matchAttribute);
+	                //TODO paperNode must currently be 'x'; howto abstract?
+	                paperNode = scoreSolution.get("x");
+	                //TODO paperResource must currently be 'x'; howto abstract?
+	                paperResource = scoreSolution.getResource("x");
+	                
+	                scoreMatch = matchNode.toString();
+	                
+	                log.trace("\nChecking for " + scoreMatch + " from " + paperNode.toString() + " in VIVO");
+	    			
+	                //Select all matching attributes from vivo store
+	    			queryString =
+						"PREFIX core: <http://vivoweb.org/ontology/core#> " +
+						"SELECT ?x " +
+						"WHERE { ?x " + coreAttribute + "\"" +  scoreMatch + "\"}";
+	    			
+	    			//TODO how to combine result sets? not possible in JENA
+	    			vivoResult = executeQuery(vivo, queryString);
+	    			commitResultSet(vivo,scoreInput,vivoResult,paperResource,paperNode,matchNode);
 	            }	    			 
+		    	
+		    	//TODO return scoreInput minus the scored statements
+		    	return scoreInput;
 		 }
 	}
